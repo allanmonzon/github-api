d15194a66496a92e0918c1381228b9a143980f64
Allan Monzon <allanjmonzon@gmail.com>
2014-09-11 17:33:04 -0400

    Fourth Commit

--- main.js
+++ main.js
@@ -1,41 +1,33 @@
-var userData;
 
-$.getJSON("https://api.github.com/users/allanmonzon").done(function(data) {
-	var models = _.map(userData, function(item) {
-		return {
-				image: item.avatar_url,
-			 home_url: item.html_url,
-				 name: item.name,
-			 username: item.login,
-			 location: item.location,
-			 	email: item.email,
-			   joined: item.created_at,
-			followers: item.followers,
-			following: item.following,
-			   orgurl: item.org_url,
-			   imgurl: item.org_img_url
-		};
-	});
+ $.getJSON("https://api.github.com/users/allanmonzon").done(function(item) {
+ 	var items = {
+ 		    image: item.avatar_url,
+ 		 home_url: item.html_url,
+ 			 name: item.name,
+ 		 username: item.login,
+ 		 location: item.location,
+ 			email: item.email,
+ 		   joined: item.created_at,
+ 	    followers: item.followers,
+ 		following: item.following,
+ 	}
 
-	_.each(models, function(model) {
-		var templateString = $('#template-user').text();
-		var templateFunction = _.template(templateString);
-		var renderTemplate = templateFunction(model);
-		$('.left').append(renderTemplate);
-	});
-
-});
+ 	var templateString = $('#template-user').text();
+ 	var templateFunction = _.template(templateString);
+ 	var renderTemplate = templateFunction(items);
+ 	$('.left').append(renderTemplate);
+ });
 
 
-
-$.getJSON("https://api.github.com/users/allanmonzon/repos").done(function(data) {
-	
+var cool = $.getJSON("https://api.github.com/users/allanmonzon/repos").done(function(data) {	
 	var models = _.map(data, function(item) {
 		return {
-			repo_name: item.name,
-			repo_url: item.html_url,
-			repo_update: item.updated_at,
-			repo_language: item.language,
+			 repo_name: item.name,
+			  repo_url: item.html_url,
+		   repo_update: item.updated_at,
+		 repo_language: item.language,
+            stargazers: item.stargazers_count,
+                 forks: item.forks_count
 		};
 	});
 
@@ -45,5 +37,27 @@ $.getJSON("https://api.github.com/users/allanmonzon/repos").done(function(data) 
 		var renderTemplate = templateFunction(model);
 		$('.right').append(renderTemplate);
 	});
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

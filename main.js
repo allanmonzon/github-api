23058c2e2c5b77e1f8c79cd752b4838b019a732f
Allan Monzon <allanjmonzon@gmail.com>
2014-09-14 20:59:35 -0400

    Final Commit

--- main.js
+++ main.js
@@ -1,44 +1,102 @@
+function renderTemplate(templateId, location, model) {
+    var templateString = $(templateId).text();
+    var templateFunction = _.template(templateString);
+    var renderedTemplate = templateFunction(model);
+    $(location).append(renderedTemplate);
+}
 
-$.getJSON("https://api.github.com/users/allanmonzon").done(function(item) {
- 	var items = {
+$.getJSON('https://api.github.com/users/allanmonzon').done(function(item) {
+  	var items = {
  		    image: item.avatar_url,
  		 home_url: item.html_url,
  			 name: item.name,
  		 username: item.login,
  		 location: item.location,
  			email: item.email,
- 		   joined: item.created_at,
+ 		   joined: moment(item.created_at).format("MMM D YYYY"),
  	    followers: item.followers,
  		following: item.following,
- 	}
-
- 	var templateString = $('#template-user').text();
- 	var templateFunction = _.template(templateString);
- 	var renderTemplate = templateFunction(items);
- 	$('.left').append(renderTemplate);
+ 	}  	
+  	$.getJSON('https://api.github.com/users/allanmonzon/starred').done(function(starredData) {
+	    items.starred = starredData.length;
+	    renderTemplate('#template-user', '.user', items);
+  	});
 });
 
 
-
-var cool = $.getJSON("https://api.github.com/users/allanmonzon/repos").done(function(data) {	
+$.getJSON("https://api.github.com/users/allanmonzon/repos").done(function(data) {	
 	var models = _.map(data, function(item) {
 		return {
 			 repo_name: item.name,
 			  repo_url: item.html_url,
-		   repo_update: item.updated_at,
+		   repo_update: moment(item.updated_at).fromNow(),
 		 repo_language: item.language,
             stargazers: item.stargazers_count,
                  forks: item.forks_count
 		};
 	});
-
 	_.each(models, function(model) {
-		var templateString = $('#template-repos').text();
-		var templateFunction = _.template(templateString);
-		var renderTemplate = templateFunction(model);
-		$('.right').append(renderTemplate);
+		renderTemplate('#template-repos', '.main-repos', model);
 	});
 });
+
+
+
+
+
+
+
+
+
+
+// $.getJSON("https://api.github.com/users/allanmonzon").done(function(item) {
+//  	var items = {
+//  		    image: item.avatar_url,
+//  		 home_url: item.html_url,
+//  			 name: item.name,
+//  		 username: item.login,
+//  		 location: item.location,
+//  			email: item.email,
+//  		   joined: moment(item.created_at).format("MMM D YYYY"),
+//  	    followers: item.followers,
+//  		following: item.following,
+//  	}
+
+//  	var templateString = $('#template-user').text();
+//  	var templateFunction = _.template(templateString);
+//  	var renderTemplate = templateFunction(items);
+//  	$('.user').append(renderTemplate);
+ 	
+// });
+
+
+
+
+
+// var cool = $.getJSON("https://api.github.com/users/allanmonzon/repos").done(function(data) {	
+// 	var models = _.map(data, function(item) {
+// 		return {
+// 			 repo_name: item.name,
+// 			  repo_url: item.html_url,
+// 		   repo_update: moment(item.updated_at).fromNow(),
+// 		 repo_language: item.language,
+//             stargazers: item.stargazers_count,
+//                  forks: item.forks_count
+// 		};
+// 	});
+
+// 	_.each(models, function(model) {
+// 		var templateString = $('#template-repos').text();
+// 		var templateFunction = _.template(templateString);
+// 		var renderTemplate = templateFunction(model);
+// 		$('.main-repos').append(renderTemplate);
+// 	});
+// });
+
+
+
+
+
 
 
 
